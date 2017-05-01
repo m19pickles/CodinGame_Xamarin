84a086d87a70df2f1373c57c06de53da59df26b6
m19pickles <Jeffkroebig@gmail.com>
2017-04-30 19:57:45 -0700

    First Pass
    I think this has way to many loops and want to go back and relook at it

--- CodinGame/E_ASCII-Art.cs
+++ CodinGame/E_ASCII-Art.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -11,28 +12,52 @@ class Solution
 		int L = int.Parse(Console.ReadLine());
 		int H = int.Parse(Console.ReadLine());
 		string T = Console.ReadLine();
+		var letterList = new List<string>();
 
 
 		for (int i = 0; i < H; i++)
 		{
 			string ROW = (Console.ReadLine());
 
-
-
-			string line = "";
-			int pos = ((char.ToUpper(T[0]) - 65) * 4);
-			for (int c = pos; c < pos + L; c++)
+			//split each line on the required number of char to create array of characters
+			int startPos = 0;
+			for (int letterGroup = 0; letterGroup < ROW.Length / L; letterGroup++)
 			{
-				line += ROW[c];
+				if (i == 0)
+				{
+					letterList.Add(ROW.Substring(startPos, L));
+				}
+				else
+				{
+					letterList[letterGroup] += ROW.Substring(startPos, L);
+				}
+				startPos += L;
 			}
-			Console.WriteLine(line);
-
-
+		}
+		//Find indexs of letters
+		var stringReconstruct = new List<string>();
+		foreach (char c in T)
+		{
+			int pos = 26;
+			if (char.ToUpper(c) >= 65 && char.ToUpper(c) <= 90)
+			{
+				pos = char.ToUpper(c) - 65;
+			}
+			//Creates list of only letters needed
+			stringReconstruct.Add(letterList[pos]);
+		}
+		string[] charOutPut = new string[H];
+		int lineNum = 0;
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = 0; j < stringReconstruct.Count; j++)
+			{
+				charOutPut[i] += stringReconstruct[j].Substring(lineNum, L);
+			}
+			lineNum += L;
+			//print output
+			Console.WriteLine("{0}", charOutPut[i]);
 		}
 
-		// Write an action using Console.WriteLine()
-		// To debug: Console.Error.WriteLine("Debug messages...");
-
-		//Console.WriteLine("answer");
 	}
 }
